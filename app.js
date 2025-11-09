99bbf0f255411bf67f25f18e80430ec5e34e9de0
AkashKumar3d <mailtoakashit@gmail.com>
2025-11-09 09:53:46 +0530

    some changes

--- app.js
+++ app.js
@@ -2,18 +2,33 @@ import express from "express";
 import dotenv from "dotenv";
 import { dbConnect } from "./database/dbconnect.js";
 import userRoutes from "./route/user.js";
+import cors from "cors";
+import path from "path";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 const app=express()
 dotenv.config();
 let port =  process.env.PORT || 4000
 
+app.use(cors())
 
 app.use(express.json());
 
 // Routes
 app.use("/api/users", userRoutes);
 
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname, "../../client/dist")));
+  
+//     app.get(/.*/, (req, res) => {
+//       res.sendFile(path.resolve(__dirname, "../../client/dist", "index.html"));
+//     });
+//   }
+
 dbConnect();
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
--- database/dbconnect.js
+++ database/dbconnect.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 export const dbConnect = async () => {
   try {
-    await mongoose.connect("mongodb+srv://mailtoakashit_db_user:XBJRdvB0u4aFnScU@cluster0.cfhcxbt.mongodb.net/", {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
